383b7a90c11da8040de6a4e3807cc81e1ad95e73
Autumn Apsey <aapsey@nexient.com>
2017-10-21 10:25:47 -0400

    Added working links to navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../index.css';
 
 const Navigation = () => (
     <div className="navigation">
         <ul>
-            <li>Home</li>
-            <li>Members</li>
-            <li>Upgrades</li>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/members'}>Members</Link></li>
+            <li><Link to={'/upgrades'}>Upgrades</Link></li>
         </ul>
     </div>
 );
